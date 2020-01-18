f4099672c15ab53a5992911dff9bac61a947bf34
xchuki00 <p.chukir@gmail.com>
2020-01-18 18:01:19 +0100

    working new sln

---
+++ init.bat
@@ -0,0 +1,5 @@
+:: Copy vcxproj files with edited paths
+:: 
+COPY ConfigForSmallUpbpSln\embree.vcxproj smallupbp\embree-2.0\embree\embree.vcxproj
+COPY ConfigForSmallUpbpSln\SmallUPBP.vcxproj smallupbp\SmallUPBP\SmallUPBP.vcxproj
+COPY ConfigForSmallUpbpSln\sys.vcxproj smallupbp\embree-2.0\embree\sys\sys.vcxproj

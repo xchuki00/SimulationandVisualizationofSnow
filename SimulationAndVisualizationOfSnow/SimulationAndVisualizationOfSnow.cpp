a911d79d5563c201d51d661c5a149f4bfe4651b8
Patrik Chukir <p.chukir@gmail.com>
2020-02-24 08:40:29 +0100

    progress

--- SimulationAndVisualizationOfSnow/SimulationAndVisualizationOfSnow.cpp
+++ SimulationAndVisualizationOfSnow/SimulationAndVisualizationOfSnow.cpp
@@ -1,20 +1,285 @@
 ﻿// SimulationAndVisualizationOfSnow.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
 //
+#pragma warning(disable: 4482)
 
 #include <iostream>
 
-int main()
+#include "src/PTBP.hxx"
+#include "Bre\EmbreeAcc.hxx"
+#include "Misc\Config.hxx"
+
+AbstractRenderer* getPTPB(
+	const Config& aConfig,
+	const int     aSeed,
+	const int     aBaseSeed) {
+
+	std::cout << "TEST: " << aConfig.mAlgorithm << std::endl;
+	const Scene& scene = *aConfig.mScene;
+	return new PTBP(
+		scene, 
+		PTBP::kCustom, 
+		aConfig.mAlgorithmFlags, 
+		aConfig.mQueryBeamType,
+		aConfig.mBB1DRadiusInitial, 
+		aConfig.mBB1DRadiusAlpha, 
+		aConfig.mBB1DRadiusCalculation, 
+		aConfig.mBB1DRadiusKNN, 
+		aConfig.mPhotonBeamType, 
+		aConfig.mBB1DUsedLightSubPathCount, 
+		aConfig.mBB1DBeamStorageFactor, 
+		aConfig.mRefPathCountPerIter, 
+		aConfig.mPathCountPerIter,
+		aConfig.mMinDistToMed,
+		aConfig.mMaxMemoryPerThread, 
+		aSeed, aBaseSeed, 
+		aConfig.mIgnoreFullySpecPaths
+	);
+
+	/*return new PTBP(scene, aSeed, PTBP::kPointBeam2D, aConfig.mPB2DRadiusInitial, aConfig.mPB2DRadiusAlpha, aConfig.mPB2DRadiusCalculation, aConfig.mPB2DRadiusKNN, aConfig.mQueryBeamType,
+		aConfig.mBB1DRadiusInitial, aConfig.mBB1DRadiusAlpha, aConfig.mBB1DRadiusCalculation, aConfig.mBB1DRadiusKNN, aConfig.mPhotonBeamType, aConfig.mBB1DUsedLightSubPathCount, aConfig.mRefPathCountPerIter);*/
+}
+//Prevzato z SmallUPBP, autor Petr Vévoda
+// Output image in continuous outputting
+void continuousOutput(const Config& aConfig, int iter, Framebuffer& accumFrameBuffer, Framebuffer& outputFrameBuffer, AbstractRenderer* renderer, const std::string& name, const std::string& ext, char* filename)
 {
-    std::cout << "Hello World!\n";
+	if (aConfig.mContinuousOutput > 0)
+	{
+		accumFrameBuffer.Add(renderer->GetFramebufferUnscaled());
+		renderer->GetFramebufferUnscaled().Clear();
+		if (iter % aConfig.mContinuousOutput == 0)
+		{
+			outputFrameBuffer.Clear();
+			outputFrameBuffer.AddScaled(accumFrameBuffer, 1.0f / iter);
+
+
+			sprintf_s(filename, 1024, "%s-%d.%s", name.c_str(), iter, ext.c_str());
+			//// Saves the image
+			outputFrameBuffer.Save(filename);
+		}
+	}
 }
 
-// Spuštění programu: Ctrl+F5 nebo nabídka Ladit > Spustit bez ladění
-// Ladění programu: F5 nebo nabídka Ladit > Spustit ladění
+//////////////////////////////////////////////////////////////////////////
+// The main rendering function, renders what is in aConfig
 
-// Tipy pro zahájení práce:
-//   1. K přidání nebo správě souborů použijte okno Průzkumník řešení.
-//   2. Pro připojení ke správě zdrojového kódu použijte okno Team Explorer.
-//   3. K zobrazení výstupu sestavení a dalších zpráv použijte okno Výstup.
-//   4. K zobrazení chyb použijte okno Seznam chyb.
-//   5. Pokud chcete vytvořit nové soubory kódu, přejděte na Projekt > Přidat novou položku. Pokud chcete přidat do projektu existující soubory kódu, přejděte na Projekt > Přidat existující položku.
-//   6. Pokud budete chtít v budoucnu znovu otevřít tento projekt, přejděte na Soubor > Otevřít > Projekt a vyberte příslušný soubor .sln.
+float render(
+	const Config& aConfig,
+	int* oUsedIterations = NULL)
+{
+	std::cout<<"TEST: " << aConfig.mScene->mMedia.size() << std::endl;
+
+	// Don't use more threads than iterations (in case of rendering limited by number of iterations not time)
+	int usedThreads = aConfig.mNumThreads;
+	if (aConfig.mMaxTime <= 0) usedThreads = std::min(usedThreads, aConfig.mIterations);
+
+	// Set number of used threads
+	omp_set_num_threads(usedThreads);
+
+	// Create 1 renderer per thread
+	typedef AbstractRenderer* AbstractRendererPtr;
+	AbstractRendererPtr* renderers;
+	renderers = new AbstractRendererPtr[usedThreads];
+
+	for (int i = 0; i < usedThreads; i++)
+	{
+		renderers[i] = getPTPB(aConfig, aConfig.mBaseSeed + i, aConfig.mBaseSeed);
+		//renderers[i] = CreateRenderer(aConfig, aConfig.mBaseSeed + i, aConfig.mBaseSeed);
+		renderers[i]->mMaxPathLength = aConfig.mMaxPathLength;
+		renderers[i]->mMinPathLength = aConfig.mMinPathLength;
+		renderers[i]->SetupDebugImages(aConfig.mDebugImages);
+		renderers[i]->SetupBeamDensity(aConfig.mBeamDensType, aConfig.mScene->mCamera.mResolution, aConfig.mBeamDensMax);
+	}
+
+	clock_t startT = clock();
+	int iter = 0;
+
+	Framebuffer accumFrameBuffer, outputFrameBuffer;
+	accumFrameBuffer.Setup(aConfig.mResolution);
+	outputFrameBuffer.Setup(aConfig.mResolution);
+	std::string name = aConfig.mOutputName.substr(0, aConfig.mOutputName.length() - 4);
+	std::string ext = aConfig.mOutputName.substr(aConfig.mOutputName.length() - 3, 3);
+	char filename[1024]; // Must be shared, otherwise critical section fails
+
+	// Rendering loop, when we have any time limit, use time-based loop,
+	// otherwise go with required iterations
+	if (aConfig.mMaxTime > 0)
+	{
+		// Time based loop
+#pragma omp parallel shared(iter,accumFrameBuffer,outputFrameBuffer,name,ext,filename)
+		while (clock() < startT + aConfig.mMaxTime * CLOCKS_PER_SEC)
+		{
+			int threadId = omp_get_thread_num();
+			renderers[threadId]->RunIteration(iter);
+
+#pragma omp critical
+			{
+				iter++; // counts number of iterations
+				continuousOutput(aConfig, iter, accumFrameBuffer, outputFrameBuffer, renderers[threadId], name, ext, filename);
+			}
+		}
+	}
+	else
+	{
+		// Iterations based loop
+		int cnt = 0, p = -1;
+#pragma omp parallel for shared(cnt,p,accumFrameBuffer,outputFrameBuffer,name,ext,filename)
+		for (iter = 0; iter < aConfig.mIterations; iter++)
+		{
+			int threadId = omp_get_thread_num();
+			renderers[threadId]->RunIteration(iter);
+#pragma omp critical
+			{
+				++cnt;
+				int percent = (int)(((float)cnt / aConfig.mIterations) * 100.0f);
+				if (percent != p)
+				{
+					p = percent;
+					std::cout << percent << "%" << std::endl;
+				}
+				continuousOutput(aConfig, cnt, accumFrameBuffer, outputFrameBuffer, renderers[threadId], name, ext, filename);
+			}
+		}
+		iter = aConfig.mIterations;
+	}
+
+	clock_t endT = clock();
+
+	if (oUsedIterations)
+		*oUsedIterations = iter + 1;
+
+	int usedRenderers = 0;
+
+	aConfig.mCameraTracingTime = 0;
+	aConfig.mBeamDensity.Setup(aConfig.mBeamDensType, aConfig.mScene->mCamera.mResolution, aConfig.mBeamDensMax);
+
+	// Not all created renderers had to have been used.
+	// Those must not participate in accumulation.
+	for (int i = 0; i < usedThreads; i++)
+	{
+		if (!renderers[i]->WasUsed())
+			continue;
+
+		if (aConfig.mContinuousOutput <= 0)
+		{
+			if (usedRenderers == 0)
+			{
+				renderers[i]->GetFramebuffer(*aConfig.mFramebuffer);
+			}
+			else
+			{
+				Framebuffer tmp;
+				renderers[i]->GetFramebuffer(tmp);
+				aConfig.mFramebuffer->Add(tmp);
+			}
+		}
+
+		renderers[i]->AccumulateDebugImages(aConfig.mDebugImages);
+		renderers[i]->AccumulateBeamDensity(aConfig.mBeamDensity);
+
+		aConfig.mCameraTracingTime += renderers[i]->mCameraTracingTime;
+
+		usedRenderers++;
+	}
+
+	if (aConfig.mContinuousOutput <= 0)
+	{
+		// Scale framebuffer by the number of used renderers
+		aConfig.mFramebuffer->Scale(1.f / usedRenderers);
+	}
+	else
+	{
+		*aConfig.mFramebuffer = accumFrameBuffer;
+		aConfig.mFramebuffer->Scale(1.f / iter);
+	}
+
+	aConfig.mCameraTracingTime /= iter;
+
+	// Clean up renderers
+	for (int i = 0; i < usedThreads; i++)
+		delete renderers[i];
+
+	delete[] renderers;
+
+	return float(endT - startT) / CLOCKS_PER_SEC;
+}
+int main(int argc, const char* argv[])
+{
+	try
+	{
+		// Warns when not using C++11 Mersenne Twister
+		PrintRngWarning();
+
+		EmbreeAcc::initLib();
+
+		// Setups config based on command line
+		Config config;
+		ParseCommandline(argc, argv, config);
+
+		// If number of threads is invalid, set 1 thread per processor
+		if (config.mNumThreads <= 0)
+			config.mNumThreads = std::max(1, omp_get_num_procs());
+
+		// When some error has been encountered, exits
+		if (config.mScene == NULL)
+			return 1;
+
+		// Sets up framebuffer
+		Framebuffer fbuffer;
+		config.mFramebuffer = &fbuffer;
+
+		// Prints what we are doing
+		printf("Scene:    %s\n", config.mScene->mSceneName.c_str());
+		if (config.mMaxTime > 0)
+			printf("Target:   %g seconds render time\n", config.mMaxTime);
+		else
+			printf("Target:   %d iteration(s)\n", config.mIterations);
+
+		// Renders the image
+		std::string desc = GetDescription(config, "            ");
+		printf("Running:  %s", desc.c_str());
+		fflush(stdout);
+		int iterations;
+		float time = render(config, &iterations);
+		EmbreeAcc::cleanupLib();
+		printf("done in %.2f s (%i iterations)\n", time, (iterations - 1));
+		if (config.mCameraTracingTime) printf("avg camera time %.2f s\n", config.mCameraTracingTime);
+
+		std::string extension = config.mOutputName.substr(config.mOutputName.length() - 3, 3);
+
+		if (config.mShowTime || config.mIterations <= 0)
+		{
+			std::ostringstream modifiedOutputName;
+
+			modifiedOutputName << config.mOutputName.substr(0, config.mOutputName.length() - 4);
+			if (config.mIterations <= 0)
+			{
+				modifiedOutputName << "_i" << iterations - 1;
+			}
+			if (config.mShowTime)
+			{
+				modifiedOutputName.precision(2);
+				modifiedOutputName << "_time" << std::fixed << time;
+			}
+			modifiedOutputName << "." << extension;
+
+			config.mOutputName = modifiedOutputName.str();
+		}
+
+		// Saves the image
+		fbuffer.Save(config.mOutputName, 2.2f /*gamma*/);
+
+		std::string name = config.mOutputName.substr(0, config.mOutputName.length() - 4);
+		config.mDebugImages.Output(name, extension);
+		config.mBeamDensity.Output(name, extension);
+
+		// Scene cleanup
+		delete config.mScene;
+
+		return 0;
+	}
+	catch (...)
+	{
+		std::cerr << "Error: unknown error" << std::endl;
+		exit(2);
+	}
+}
---
+++ SimulationAndVisualizationOfSnow/src/PTPBBeams.cxx
@@ -0,0 +1,229 @@
+#include "PTBPBeams.hxx"
+#include "Misc\Timer.hxx"
+#include "Misc\KdTmpl.hxx"
+
+#ifdef USE_BRUTE
+#include "PhBrute.hxx"
+#endif
+
+#ifdef USE_EMBREE
+#include "PhEmbree.hxx"
+#endif
+
+#ifdef USE_GRID
+#include "..\..\smallupbp\SmallUPBP\src\Beams\PhGrid.hxx"
+#endif
+
+/**
+ * @brief	Defines an alias representing a kd tree for knn queries.
+ */
+typedef KdTreeTmplPtr< Pos, Pos > KdTree;
+
+const float MAX_FLOAT_SQUARE_ROOT = std::sqrtf(std::numeric_limits< float >::max()); //!< The maximum float square root
+
+uint PTBPEvaluator::sGridSize = 256;     //!< Size of the grid.
+uint PTBPEvaluator::sMaxBeamsInCell = 0; //!< Maximum number of tested beams in a single cell. 0 means no restriction.
+uint PTBPEvaluator::sReductionType = 0;  //!< Type of the reduction of numbers of tested beams in cells.	
+ 
+// ----------------------------------------------------------------------------------------------
+
+/**
+ * @brief	Builds the data structure for Beam-Beam queries on the given beams.
+ *
+ * @param [in,out]	beams	 	The beams.
+ * @param	radiusCalculation	Type of radius calculation.
+ * @param	beamRadius		 	Beam radius.
+ * @param	knn				 	Value x means that x-th closest beam vertex will be used for
+ * 								calculation of cone radius at the current beam vertex.
+ * @param	verbose			 	Whether to print information about progress.
+ */
+void PTBPEvaluator::build(PhotonBeamsArray & beams,
+	RadiusCalculation radiusCalculation,
+	const float beamRadius,
+	const int knn,
+	bool verbose)
+{	
+	const float SMALLEST_RADIUS = 0.001f;
+
+	UPBP_ASSERT(accelStruct == nullptr);
+	UPBP_ASSERT( !beams.empty() );
+
+	KdTree * tree;
+	KdTree::CKNNQuery * query;
+	if (radiusCalculation == KNN_RADIUS)
+	{
+		tree = new KdTree();
+		tree->Reserve(beams.size());
+		for (int i = 0; i < (int)beams.size(); i++)
+		{
+			tree->AddItem((Pos *)(&beams[i].mRay.origin), i);
+		}
+		tree->BuildUp();
+		query = new KdTree::CKNNQuery(knn);
+	}
+
+	// Define radius of each beam
+	for (PhotonBeamsArray::iterator it = beams.begin(); it != beams.end(); ++it)
+	{
+		if (radiusCalculation == KNN_RADIUS)
+		{
+			// Query for beam start
+			query->Init(it->mRay.origin, knn, MAX_FLOAT_SQUARE_ROOT);
+			tree->KNNQuery(*query, tree->truePred);
+			UPBP_ASSERT(query->found > 1);
+			it->mStartRadius = std::max(2.0f * sqrtf(query->dist2[1]) * beamRadius,SMALLEST_RADIUS);
+			// Query for beam end
+			query->Init(it->mRay.target(it->mLength), knn, MAX_FLOAT_SQUARE_ROOT);
+			tree->KNNQuery(*query, tree->truePred);
+			UPBP_ASSERT(query->found > 1);
+			it->mEndRadius = std::max(2.0f * sqrtf(query->dist2[1]) * beamRadius, SMALLEST_RADIUS);
+			float maxRadius = std::max(it->mStartRadius, it->mEndRadius);
+			it->mMaxRadiusSqr = maxRadius * maxRadius;
+			it->mRadiusChange = (it->mEndRadius - it->mStartRadius) / it->mLength;
+		}
+		else
+		{			
+			it->mStartRadius = it->mEndRadius = beamRadius;
+			it->mMaxRadiusSqr = beamRadius * beamRadius;
+			it->mRadiusChange = 0.0f;
+		}
+		it->mInvocation = 0; // Not yet intersected
+	}
+
+	if (radiusCalculation == KNN_RADIUS)
+	{
+		delete query;
+		delete tree;
+	}
+
+	Timer timer;
+	timer.Start();
+
+
+	accelStruct = new AccelStruct();
+	UPBP_ASSERT(accelStruct != nullptr);
+#ifdef USE_GRID
+	accelStruct->setGridSize(sGridSize);
+	accelStruct->setMaxBeamsInCell(sMaxBeamsInCell);
+	accelStruct->setReductionType(sReductionType);
+	accelStruct->setSeed(mSeed);
+#endif
+	accelStruct->build(beams, verbose);
+
+	timer.Stop();
+	const double treeConstructionTime = timer.GetLastElapsedTime();
+
+	if(verbose) 
+	{
+		std::cout
+			<< std::setprecision(3)
+			<< "   - Beam struct took  " << treeConstructionTime << " sec.\n" << std::endl;
+	}
+}
+
+// ----------------------------------------------------------------------------------------------
+
+/**
+ * @brief	Destroys the data structure for Beam-Beam queries.
+ */
+void PTBPEvaluator::destroy( ) 
+{
+	delete accelStruct;
+	accelStruct = nullptr;
+}
+
+// ----------------------------------------------------------------------------------------------
+
+/**
+ * @brief	Evaluates the beam-beam estimate for the given query ray.
+ *
+ * @param	beamType					   	Type of the beam.
+ * @param	queryRay					   	The query ray (=beam) for the Beam-beam estimate.
+ * @param	segments					   	Full volume segments of media intersected by the ray.
+ * @param	estimatorTechniques			   	The estimator techniques to use.
+ * @param	raySamplingFlags			   	The ray sampling flags (\c AbstractMedium::kOriginInMedium).
+ * @param [in,out]	additionalRayDataForMis	(Optional) additional data needed for MIS weights
+ * 											computations.
+ * @param [in,out]	gridStats			   	(Optional) statistics to gather for the ray.
+ *
+ * @return	The accumulated radiance along the ray.
+ */
+Rgb PTBPEvaluator::evalBeamBeamEstimate(
+	BeamType beamType,
+	const Ray& queryRay,
+	const VolumeSegments& segments,
+	const uint estimatorTechniques,
+	const uint raySamplingFlags,
+	embree::AdditionalRayDataForMis* additionalRayDataForMis,
+	GridStats* gridStats)
+{
+	UPBP_ASSERT(estimatorTechniques & BB1D);
+	UPBP_ASSERT(raySamplingFlags == 0 || raySamplingFlags == AbstractMedium::kOriginInMedium);
+	
+	Rgb result(0);
+
+	Rgb attenuation(1);
+	float raySamplePdf = 1.0f;
+	float raySampleRevPdf = 1.0f;
+	GridStats _gridStats;
+	if (!gridStats) gridStats = &_gridStats;
+
+	/// Accumulate for each segment
+	for (VolumeSegments::const_iterator it = segments.begin(); it != segments.end(); ++it)
+	{
+		// Get segment medium
+		const AbstractMedium * medium = scene.mMedia[it->mMediumID];
+		
+		// Accumulate
+		Rgb segmentResult(0);
+		if (medium->HasScattering())
+		{
+			if (additionalRayDataForMis)
+			{
+				additionalRayDataForMis->mRaySamplePdf = raySamplePdf;
+				additionalRayDataForMis->mRaySampleRevPdf = raySampleRevPdf;
+				additionalRayDataForMis->mRaySamplingFlags = AbstractMedium::kEndInMedium;
+				if (it == segments.begin())
+					additionalRayDataForMis->mRaySamplingFlags |= raySamplingFlags;
+			}
+			segmentResult = accelStruct->evalBeamBeamEstimate(queryRay, beamType | estimatorTechniques, medium, it->mDistMin, it->mDistMax, *gridStats, additionalRayDataForMis);
+		}
+		// Add to total result
+		result += attenuation * segmentResult;
+
+		if (additionalRayDataForMis)
+		{
+			DebugImages & debugImages = *static_cast<DebugImages *>(additionalRayDataForMis->mDebugImages);
+			debugImages.accumRgb2ToRgb(DebugImages::BB1D, attenuation);
+			debugImages.ResetAccum2();
+		}
+
+		// Update attenuation
+		attenuation *= beamType == SHORT_BEAM ? it->mAttenuation / it->mRaySamplePdf :  // Short beams - no attenuation
+			it->mAttenuation;
+		if (!attenuation.isPositive())
+			return result;
+
+		// Update PDFs
+		raySamplePdf *= it->mRaySamplePdf;
+		raySampleRevPdf *= it->mRaySampleRevPdf;
+	}
+
+	UPBP_ASSERT(!result.isNanInfNeg());
+
+	return result;
+}
+
+
+/**
+ * @brief	Gets probability of selecting a beam (in case of beam reduction) around the given
+ * 			position.
+ *
+ * @param	pos	The position.
+ *
+ * @return	The beam selection PDF.
+ */
+float PTBPEvaluator::getBeamSelectionPdf(const Pos & pos) const
+{
+	return accelStruct->getBeamSelectionPdf(pos);
+}
